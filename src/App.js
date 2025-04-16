9b0b5add9baf11564fa1e7c82839db347e7dd5f0
Aditya-138-12 <adityasaroha456@gmail.com>
2025-04-16 10:15:07 +0530

    Changed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <span>This is confess india, the largest Indian Social platform.</span>
+  );
+}
+
+export default App;
